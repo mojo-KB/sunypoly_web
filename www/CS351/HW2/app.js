ec987443b0f482ff128045af44824f60d2b6bba5
mojo-KB <56409448+mojo-KB@users.noreply.github.com>
2022-02-14 16:08:00 -0500

    comment out the not-started homeworks

--- www/CS351/HW2/app.js
+++ www/CS351/HW2/app.js
@@ -4,3 +4,12 @@
 
 
 const list = [];
+// WRITE YOUR CODE IN HERE:
+
+let button = document.createElement("button");
+
+let div = document.querySelector('div');
+
+for (let i = 0; i < 100; i++) {
+    div.appendChild(button);
+}
