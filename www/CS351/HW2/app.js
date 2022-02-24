ffe899af1b4c3a54808229514758bd33ad4f41df
mojo-KB <56409448+mojo-KB@users.noreply.github.com>
2022-02-23 19:00:40 -0500

    update reset button

--- www/CS351/HW2/app.js
+++ www/CS351/HW2/app.js
@@ -2,13 +2,16 @@ const form = document.querySelector('#main_form');
 const list = document.querySelector('#input_list');
 const button = document.querySelector('button');
 const selectList = document.querySelector('#category_list');
+const resetBtn = document.querySelector('#reset');
 
 
 window.addEventListener("change", () => loadEdit(), false);
 
 async function loadEdit() {
     // do the await things here.
+    document.querySelector('#default').hidden = true;
     document.querySelector('#userInput').disabled = false;
+    document.querySelector('#category_list').disabled = true;
     document.querySelector('#userInput').setAttribute("placeholder", `Enter your favorite ${selectList.value}`)
     form.addEventListener('submit', (e) => {
         e.preventDefault();
@@ -18,4 +21,18 @@ async function loadEdit() {
         list.append(newList);
         input.value = '';
     })
+
+    resetBtn.addEventListener('click', (e) => {
+        e.preventDefault();
+        document.querySelector('#userInput').disabled = true;
+        document.querySelector('#category_list').disabled = false;
+        document.querySelector('#userInput').setAttribute("placeholder", `Please choose a category`)
+        removeAllChildNodes(list);
+    })
+}
+
+function removeAllChildNodes(parent) {
+    while (parent.firstChild) {
+        parent.removeChild(parent.firstChild);
+    }
 }
