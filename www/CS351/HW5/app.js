93a01b113615041858dc06acf4eb2cdbf10bbb1f
mojo-KB <56409448+mojo-KB@users.noreply.github.com>
2022-03-09 13:36:08 -0500

    change to html default

--- www/CS351/HW5/app.js
+++ www/CS351/HW5/app.js
@@ -1,30 +1,5 @@
-// document.getElementById("myinputred").oninput = function() {
-//     var value = (this.value - this.min) / (this.max - this.min) * 100
-//     this.style.background = 'linear-gradient(to right, #ff0000 0%, #ff0000 ' + value + '%, #fff ' + value + '%, white 100%)'
-// };
-// document.getElementById("myinputgreen").oninput = function() {
-//     var value = (this.value - this.min) / (this.max - this.min) * 100
-//     this.style.background = 'linear-gradient(to right, #00ff00 0%, #00ff00 ' + value + '%, #fff ' + value + '%, white 100%)'
-// };
-// document.getElementById("myinputblue").oninput = function() {
-//     var value = (this.value - this.min) / (this.max - this.min) * 100
-//     this.style.background = 'linear-gradient(to right, #00ff 0%, #00f ' + value + '%, #fff ' + value + '%, white 100%)'
-// };
+let b = document.getElementById('btn');
 
-
-const button = document.querySelector('button');
-const h1 = document.querySelector('h1');
-
-
-button.addEventListener('click', function() {
-    const newColor = randomColor();
-    document.body.style.backgroundColor = newColor;
-    h1.innerText = newColor;
+b.addEventListener('submit', () => {
+    console.log(document.getElementById('red-range').value);
 })
-
-const randomColor = () => {
-    const r = Math.floor(Math.random() * 255);
-    const g = Math.floor(Math.random() * 255);
-    const b = Math.floor(Math.random() * 255);
-    return `rgb(${r}, ${g}, ${b})`;
-}
